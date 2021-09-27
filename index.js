8174866035e9e91b54cc189d2342ece7fe74bd3d
jeraldpraveen <jerald.vit@gmail.com>
2021-09-27 23:56:51 +0530

    Bringing Up Screen in http://localhost:5000/

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+//Bringing Up Screen in http://localhost:5000/
+app.use("/", (req, res) => {
+  res.json("Welcome to Node.js");
+});
+
+//Listen Port
 app.listen(5000, () => {
   console.log("Server Started On 5000");
 });
